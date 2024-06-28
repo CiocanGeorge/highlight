c33d5867584690c3889efba9c750e5befe30d3c5
CIOCANGEORGE\cioca <ciocan_george97@yahoo.com>
2024-06-28 13:30:50 +0300

    test

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Generate Video Highlights</title>
+    <title>Upload Video</title>
 </head>
 <body>
-    <h1>Generate Video Highlights from YouTube</h1>
-    <form action="process.php" method="post">
-        <label for="youtube_link">YouTube Link:</label>
-        <input type="text" id="youtube_link" name="youtube_link" required>
-        <button type="submit">Generate Highlights</button>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select video to upload:
+        <input type="file" name="video" id="video">
+        <input type="submit" value="Upload Video" name="submit">
     </form>
 </body>
 </html>
