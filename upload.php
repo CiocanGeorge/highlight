36750684705f77f0f3495aa56528fb7c0634fa0f
CIOCANGEORGE\cioca <ciocan_george97@yahoo.com>
2024-06-28 13:36:06 +0300

    test

--- upload.php
+++ upload.php
@@ -1,10 +1,12 @@
 <?php
+echo "ajunge aici";
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['video'])) {
+    echo "ajunge aici2";
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["video"]["name"]);
     $uploadOk = 1;
     $videoFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
+    echo "ajunge aici3";
     // Check if video file is a actual video or fake video
     $check = getimagesize($_FILES["video"]["tmp_name"]);
     if ($check !== false) {
@@ -14,25 +16,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['video'])) {
         echo "File is not a video.";
         $uploadOk = 0;
     }
-
+    echo "ajunge aici4";
     // Check file size
     if ($_FILES["video"]["size"] > 500000000) { // 500MB
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
-
+    echo "ajunge aici5";
     // Allow certain file formats
     if ($videoFileType != "mp4" && $videoFileType != "avi" && $videoFileType != "mov"
     && $videoFileType != "mpeg") {
         echo "Sorry, only MP4, AVI, MOV & MPEG files are allowed.";
         $uploadOk = 0;
     }
-
+    echo "ajunge aici6";
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
+        echo "ajunge aici7";
         echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
+        echo "ajunge aici8";
         if (move_uploaded_file($_FILES["video"]["tmp_name"], $target_file)) {
             echo "The file ". htmlspecialchars( basename( $_FILES["video"]["name"])). " has been uploaded.";
             // Call the function to create highlight
